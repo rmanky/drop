b72db22e93d8e32ab1129466be143dcb640a294c
rmanky <rmankaryous12@gmail.com>
2022-04-29 10:03:03 -0400

    2 files modified

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
 	import 'uno.css';
 	import '@unocss/reset/tailwind.css';
-	import { title } from '$lib/stores.ts';
 </script>
 
-<svelte:head>
-	<title>{$title}</title>
-</svelte:head>
-
-<header flex="~" justify-center py-4>
-	<h1 text-3xl font-bold>{$title}</h1>
-</header>
-
 <main>
-<slot />
+	<slot />
 </main>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
-  import { title } from '$lib/stores.ts';
-  title.set('RMANKY');
+  const links = [
+    {
+      name: "Drop",
+      href: "/drop",
+      color: "#1133BB"
+    },
+    {
+      name: "Wordle",
+      href: "/wordle",
+      color: "#118833"
+    }
+  ]
 </script>
 
 
 <template>
-  <a href="/drop">Drop</a>
-  <br />
-  <a href="/wordle">Wordle</a>
+  <div w-full h-full flex items-center justify-center font-mono>
+    <div>
+      <h1 text-24 text-middle>rmanky</h1>
+      <div flex flex-col gap-2 text-6 justify-start>
+        {#each links as link}
+          <div flex flex-row>
+            <a ml-2 w="1/2" bg="green-600"
+            px-2 text-white transition-all
+            hover:w-full focus:w-full
+            style="background: {link.color}"
+            href={link.href}>> {link.name}</a>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
 </template>
